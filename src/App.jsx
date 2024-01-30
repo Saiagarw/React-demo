f793e840ac021f4a6a795954ceb86ad3cfa7f2b0
saiagarw <sailesh.agarwal@publicissapient.com>
2024-01-30 11:30:32 +0530

    sailesh

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
     return (
         <div>
-              <h1>Hello</h1>
+                <h1>Hello</h1>
         </div>
     );
 }
